84e3d6d9764bb89f017388cca7b82252ee513ec0
Vinícius Krolow <krolow@gmail.com>
2017-05-11 22:45:26 -0300

    Add idea of how presenter should looks like

---
+++ src/presenter/post.js
@@ -0,0 +1,30 @@
+module.exports = function postPresenter(data) {
+  if (Array.isArray(data)) {
+    return list(data);
+  }
+
+  return view(data);
+}
+
+
+function list(posts) {
+  return {
+    list: posts.map(view),
+    _links: {
+      self: '/posts',
+      next: '/posts?page=2',
+    }
+  }
+}
+
+
+function view(post) {
+  return {
+    id: post.id,
+    title: post.title,
+    body: post.body,
+    _links: {
+      self: `/posts/${post.id}`,
+    },
+  };
+}
