1c5a25025aa6efbe0068e199e2a6e1503696cd6a
Vinícius Krolow <krolow@gmail.com>
2018-03-03 11:14:39 -0300

    Update knex config

--- config/knex.js
+++ config/knex.js
@@ -2,13 +2,7 @@ const { env } = process;
 
 module.exports = {
   client: 'pg',
-  connection: {
-    host: env.DB_HOST || '127.0.0.1',
-    port: env.DB_PORT || '5432',
-    user: env.DB_USER,
-    password: env.DB_PASSWORD,
-    database: env.DB_DATABASE,
-  },
+  connection: getConnection(),
   pool: {
     min: 2,
     max: 10,
@@ -20,5 +14,21 @@ module.exports = {
   seeds: {
     directory: `${__dirname}/../db/seeds/`,
   },
-  debug: env.NODE_ENV === 'development',
+  debug: env.NODE_ENV !== 'production',
+  ssl: env.NODE_ENV === 'production',
 };
+
+
+function getConnection() {
+  if (env.DATABASE_URL) {
+    return env.DATABASE_URL;
+  }
+
+  return {
+    host: env.DB_HOST || '127.0.0.1',
+    port: env.DB_PORT || '5432',
+    user: env.DB_USER,
+    password: env.DB_PASSWORD,
+    database: env.DB_DATABASE,
+  };
+}
