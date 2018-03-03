7f1ab134b5c42f88aeaeb85a853d52696fa69c39
Vinícius Krolow <krolow@gmail.com>
2018-03-03 11:39:04 -0300

    Fix files loader

--- src/files-loader.js
+++ src/files-loader.js
@@ -1,21 +1,27 @@
 const requireDirAsMap = require('require-dir-as-map');
+
+
 module.exports = function loader(directory) {
   return requireDirAsMap(addEndSlashIfNeeded(directory), {
     filter: filterFile.bind(null, directory),
   });
 };
 
+
 function filterFile(directory, file) {
   return isDifferentThanIndexFile(directory, file);
 }
+
 
 function isDifferentThanIndexFile(directory, file) {
   return file !== 'index.js';
 }
 
+
 function addEndSlashIfNeeded(directory) {
-  if (directory.endsWith('/'))
+  if (directory.endsWith('/')) {
     return directory;
   }
+
   return `${directory}/`;
 }
